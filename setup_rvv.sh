a92447cf254c0f948c7549bd2baf43923850e6c8
Yoshinori Watanabe <yoshinori.watanabe.j3h@jp.denso.com>
2024-08-21 11:45:30 +0900

    compile sample program in usr/ linker failed

--- setup_rvv.sh
+++ setup_rvv.sh
@@ -1,10 +1,14 @@
+
 CCOPT="-march=rv64gcv -ftree-vectorize -O2 "
+
 export RISCV=$HOME/Work/riscv/rvv64/bin
 export CC="$RISCV/riscv64-unknown-elf-gcc $CCOPT"
 export CXX="$RISCV/riscv64-unknown-elf-g++ $CCOPT"
 export LD="$RISCV/riscv64-unknown-elf-ld $CCOPT"
 mkdir -p build && cd build
+
 #make makefile
-cmake .. -DBUILD_BENCHMARKS=OFF -DWITH_OPENMP=OFF -DBUILD_UNITTESTS=OFF -DBUILD_STATIC=ON -DBUILD_SHARED=OFF -DCMAKE_INSTALL_PREFIX=.
+cmake .. -DBUILD_BENCHMARKS=OFF -DWITH_OPENMP=OFF -DBUILD_UNITTESTS=OFF -DBUILD_STATIC=ON -DBUILD_SHARED=OFF -DCMAKE_INSTALL_PREFIX=. 
 #make
-#usr
+#cmake .. -DCMAKE_INSTALL_PREFIX=.
+#make install 
